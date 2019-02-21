c9ca42cb55f84d29a6a5364b29d070bdb279e3be
GabrielElkadiki <46852673+GabrielElkadiki@users.noreply.github.com>
2019-02-21 15:40:53 -0500

    Compartmentalized and fixed bad API responses

--- Main.py
+++ Main.py
@@ -5,18 +5,59 @@ import sys
 import os
 
 
-def getCurrentData(API_URL, symbol, data):
+def round_float(string):
+    return round(float(string), 3)
+
+
+def get_current_raw_cata(API_URL, symbol, data):
     try:
-        response = requests.get(API_URL)
+        response = requests.get(API_URL, data)
         print(response.status_code)
-        data = response.json()
+        json = response.json()
     except requests.exceptions.RequestException as e:
         print("Error: {}".format(e))
         sys.exit(1)
-    rawRecentData = (data['Time Series (Daily)'])
-    keys = (rawRecentData.keys())
+    raw_recent_data = (json['Time Series (Daily)'])
+    keys = (raw_recent_data.keys())
     print("Stock Ticker: " + symbol.upper())
-    return rawRecentData, keys
+    return raw_recent_data, keys
+
+
+def extract_data(raw_recent_data, keys):
+    recent_data = []
+    line_count = 0
+    oldest_year = 0
+    current_year = 0
+    for key in keys:
+        if line_count == 0:
+            current_year = key.split("-")[0]
+        else:
+            recent_data.append(
+                [key, round_float(raw_recent_data[key]['1. open']), round_float(raw_recent_data[key]['4. close'])])
+        line_count += 1
+    if not os.path.exists("./Historic_Data/" + str(symbol) + ".csv"):
+        print("ERROR: Specified stock ticker historic data is unavailable")
+    else:
+        csv_file = open("./Historic_Data/" + str(symbol) + ".csv")
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        line_count = 0
+        historic_data = []
+        for row in csv_reader:
+            if line_count == 1:
+                oldest_year = row[0].split("-")[0]
+            elif line_count != 0:
+                historic_data.append([row[0], round_float(row[1]), round_float(row[4])])
+            line_count += 1
+        full_data = sorted(set(tuple(i) for i in (recent_data + historic_data)))
+        year_span_size = int(current_year) - int(oldest_year) + 1
+        year_span = []
+        new_data_list = []
+        for data_point in full_data:
+            date_split = data_point[0].split("-")
+            new_data_list.append([date_split[0], date_split[1], date_split[2], (data_point[1]), (data_point[2])])
+        for i in range(year_span_size):
+            year_span.append(int(oldest_year) + i)
+        return new_data_list, year_span
 
 
 API_URL = "https://www.alphavantage.co/query"
@@ -26,33 +67,17 @@ data = {"function": "TIME_SERIES_DAILY",
         "symbol": symbol,
         "outputsize": "compact",
         "apikey": "XXX"}
-rawRecentData, keys = getCurrentData(API_URL, symbol, data)
-oldestYear = 0
-currentYear = 0
-recentData = []
-lineCount = 0
-for key in keys:
-    if lineCount == 0:
-        currentYear = key.split("-")[0]
-    else:
-        recentData.append([key, round(float(rawRecentData[key]['1. open']), 3), round(float(rawRecentData[key]['4. close']), 3)])
-    lineCount += 1
-if not os.path.exists("./Historic_Data/" + str(symbol) + ".csv"):
-    print("END: Specified stock ticker historic data is unavailable")
-else:
-    csv_file = open("./Historic_Data/" + str(symbol) + ".csv")
-    csv_reader = csv.reader(csv_file, delimiter=',')
-    lineCount = 0
-    historicData = []
-    for row in csv_reader:
-        if lineCount == 1:
-            oldestYear = row[0].split("-")[0]
-        elif lineCount != 0:
-            historicData.append([row[0], round(float(row[1]), 3), round(float(row[4]), 3)])
-        lineCount += 1
-    fullData = sorted(set(tuple(i)for i in (recentData + historicData)))
-    yearSpanSize = int(currentYear) - int(oldestYear) + 1
-    yearSpan = []
-    for i in range(yearSpanSize):
-        yearSpan.append(int(oldestYear) + i)
-    print(yearSpan)
+rawRecentData, keys = get_current_raw_cata(API_URL, symbol, data)
+dataList, yearSpan = extract_data(rawRecentData, keys)
+sortedDataList = []
+tempDataList = []
+yearCount = 0
+for year in yearSpan:
+    for data in dataList:
+        if int(data[0]) == int(year):
+            tempDataList.append(data)
+    sortedDataList.append(tempDataList)
+    tempDataList = []
+    yearCount += 1
+for data in sortedDataList:
+    print(sortedDataList[0])
