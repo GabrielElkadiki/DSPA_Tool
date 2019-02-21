c2f9ea6b3e7564ece5efba53718b9fabb4ce68df
GabrielElkadiki <46852673+GabrielElkadiki@users.noreply.github.com>
2019-02-21 13:34:43 -0500

    Revamp Backup

--- Main.py
+++ Main.py
@@ -1,5 +1,24 @@
+import matplotlib.pyplot as plt
 import requests
 import csv
+import sys
+import os
+
+
+def getCurrentData(API_URL, symbol, data):
+    try:
+        response = requests.get(API_URL)
+        print(response.status_code)
+        data = response.json()
+    except requests.exceptions.RequestException as e:
+        print("Error: {}".format(e))
+        sys.exit(1)
+    rawRecentData = (data['Time Series (Daily)'])
+    keys = (rawRecentData.keys())
+    print("Stock Ticker: " + symbol.upper())
+    return rawRecentData, keys
+
+
 API_URL = "https://www.alphavantage.co/query"
 symbol = 'aapl'
 monthRange = ["1", "2"]
@@ -7,28 +26,33 @@ data = {"function": "TIME_SERIES_DAILY",
         "symbol": symbol,
         "outputsize": "compact",
         "apikey": "XXX"}
-response = requests.get(API_URL, data)
-data = response.json()
-print("Stock Ticker: " + symbol.upper())
-rawRecentData = (data['Time Series (Daily)'])
-keys = (rawRecentData.keys())
+rawRecentData, keys = getCurrentData(API_URL, symbol, data)
 oldestYear = 0
 currentYear = 0
 recentData = []
+lineCount = 0
 for key in keys:
-    recentData.append(key + " " + rawRecentData[key]['1. open'] + " " + rawRecentData[key]['4. close'])
-csv_file = open("./Historic_Data/" + str(symbol) + ".csv")
-csv_reader = csv.reader(csv_file, delimiter=',')
-line_count = 0
-historicData = []
-for row in csv_reader:
-    if line_count == 1:
-        oldestYear = row[0].split("-")[0]
-    historicData.append(row[0] + " " + row[1] + " " + row[4])
-    line_count += 1
-fullData = list(set(recentData + historicData))
-for i in range(len(fullData)):
-    fullDataEntry = fullData[i].split(" ")
-    fullData[i] = [fullDataEntry[0], fullDataEntry[1], fullDataEntry[2]]
-for data in fullData:
-    print(data)
+    if lineCount == 0:
+        currentYear = key.split("-")[0]
+    else:
+        recentData.append([key, round(float(rawRecentData[key]['1. open']), 3), round(float(rawRecentData[key]['4. close']), 3)])
+    lineCount += 1
+if not os.path.exists("./Historic_Data/" + str(symbol) + ".csv"):
+    print("END: Specified stock ticker historic data is unavailable")
+else:
+    csv_file = open("./Historic_Data/" + str(symbol) + ".csv")
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    lineCount = 0
+    historicData = []
+    for row in csv_reader:
+        if lineCount == 1:
+            oldestYear = row[0].split("-")[0]
+        elif lineCount != 0:
+            historicData.append([row[0], round(float(row[1]), 3), round(float(row[4]), 3)])
+        lineCount += 1
+    fullData = sorted(set(tuple(i)for i in (recentData + historicData)))
+    yearSpanSize = int(currentYear) - int(oldestYear) + 1
+    yearSpan = []
+    for i in range(yearSpanSize):
+        yearSpan.append(int(oldestYear) + i)
+    print(yearSpan)
