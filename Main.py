96c0de941ac8a7bcc90d3e25e108e0fd6cc0adeb
GabrielElkadiki <46852673+GabrielElkadiki@users.noreply.github.com>
2019-03-08 19:11:22 -0500

    Redesigned list manipulation

--- Main.py
+++ Main.py
@@ -1,3 +1,4 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
 import requests
 import datetime
 import time
@@ -45,7 +46,10 @@ def extract_data(raw_recent_data, keys):
             if line_count != 0:
                 historic_data.append([row[0], round_float(row[1]), round_float(row[4])])
             line_count += 1
-        full_data = sorted(set(tuple(i) for i in (recent_data + historic_data)))
+        full_data_list_tuple = list(set(tuple(i) for i in (recent_data + historic_data)))
+        full_data = []
+        for data in full_data_list_tuple:
+            full_data.append([data[0], data[1], data[2]])
         return full_data
 
 
@@ -56,31 +60,38 @@ def get_year_span(data_list):
     year_span = []
     for i in range(year_span_size):
         year_span.append(int(oldest_year) + i)
+    return year_span
 
 
 def apply_month_range(data_list, month_range):
     filtered_data_list = []
-    if not month_range:
-        for data_point in data_list:
-            filtered_data_list.append([data_point[0], (data_point[1]), (data_point[2])])
-    else:
-        for data_point in data_list:
-            if data_point[0].split("-")[1] in month_range:
-                filtered_data_list.append([data_point[0], (data_point[1]), (data_point[2])])
+    for data_point in data_list:
+        if data_point[0].split("-")[1] in month_range:
+            filtered_data_list.append([data_point[0], data_point[1]])
     return filtered_data_list
 
 
 def max_delta(data_list):
     max_increase = [0, ""]
     max_decrease = [0, ""]
-    for data_point in data_list:
-        delta = 100 * (data_point[2] - data_point[1]) / data_point[1]
-        if delta > max_increase[0]:
-            max_increase = [delta, data_point[0]]
-        if delta < max_decrease[0]:
-            max_decrease = [delta, data_point[0]]
-        max_increase[0] = round_float(max_increase[0])
-        max_decrease[0] = round_float(max_decrease[0])
+    if len(data_list[0]) == 2:
+        for data_point in data_list:
+            delta = data_point[1]
+            if delta > max_increase[0]:
+                max_increase = [delta, data_point[0]]
+            if delta < max_decrease[0]:
+                max_decrease = [delta, data_point[0]]
+            max_increase[0] = round_float(max_increase[0])
+            max_decrease[0] = round_float(max_decrease[0])
+    else:
+        for data_point in data_list:
+            delta = 100 * (data_point[2] - data_point[1]) / data_point[1]
+            if delta > max_increase[0]:
+                max_increase = [delta, data_point[0]]
+            if delta < max_decrease[0]:
+                max_decrease = [delta, data_point[0]]
+            max_increase[0] = round_float(max_increase[0])
+            max_decrease[0] = round_float(max_decrease[0])
     print("Max Increase = " + str(max_increase[0]) + " On " + max_increase[1])
     print("Max Decrease = " + str(max_decrease[0]) + " On " + max_decrease[1])
 
@@ -88,7 +99,8 @@ def max_delta(data_list):
 def produce_final_data_list(API_URL, data):
     month_range = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12']
     raw_recent_data, keys = get_current_raw_data(API_URL, symbol, data)
-    data_list = apply_month_range(extract_data(raw_recent_data, keys), month_range)
+    data_list = extract_data(raw_recent_data, keys)
+    convert_price_to_delta(apply_month_range(data_list, month_range))
     data_list.sort(key=lambda date: time.mktime(time.strptime(date[0], "%Y-%m-%d")))
     return data_list
 
@@ -105,17 +117,16 @@ def print_monthly_maximum_delta(data_list):
 
 
 def convert_price_to_delta(data_list):
-    if len(data_list) == 2:
-        return data_list
-    for data_point in data_list:
-        delta = 100 * (data_point[2] - data_point[1]) / data_point[1]
-        data_point[1] = delta
-        del(data_point[2])
-    return data_list
+    if len(data_list[0]) == 2:
+        return
+    else:
+        for data_point in data_list:
+            delta = 100 * (data_point[2] - data_point[1]) / data_point[1]
+            data_point[1] = round_float(delta)
+            del (data_point[2])
 
 
 def compare_past_dates(data_list, num_days):
-    data_list = convert_price_to_delta(data_list)
     target_date_list = []
     for i in range(num_days):
         target_date = (datetime.date.today() + datetime.timedelta(days=i))
@@ -134,24 +145,30 @@ def compare_past_dates(data_list, num_days):
                 same_date_list.append(data)
         target_delta_list.append(same_date_list)
         same_date_list = []
-
     for target_list in target_delta_list:
+        date = ""
         pos_count = 0
         neg_count = 0
-        date = ""
+        pos_delta_sum = 0
+        neg_delta_sum = 0
         for target in target_list:
             date = target[0].split("-")[1] + "-" + (target[0]).split("-")[2]
             if target[1] >= 0:
                 pos_count += 1
+                pos_delta_sum += target[1]
             else:
                 neg_count += 1
+                neg_delta_sum += target[1]
         pbty_up = 100 * pos_count / (pos_count + neg_count)
         pbty_down = 100 * neg_count / (pos_count + neg_count)
         print("On " + date + ": ")
         print("PBTY of increase = " + str(round_float(pbty_up)) + "%")
+        print("Average increase change = " + str(round_float(pos_delta_sum / pos_count)))
         print("PBTY of decrease = " + str(round_float(pbty_down)) + "%")
+        print("Average decrease change = " + str(round_float(neg_delta_sum / pos_count)))
         print("Number of years = " + str(pos_count + neg_count))
         print("________________________________________________")
+
 
 def main():
     API_URL = "https://www.alphavantage.co/query"
@@ -160,13 +177,12 @@ def main():
             "outputsize": "compact",
             "apikey": "XXX"}
     dataList = produce_final_data_list(API_URL, data)
+    convert_price_to_delta(dataList)
     # max_delta(dataList)
     # print_monthly_maximum_delta(dataList)
-    # print(convert_price_to_delta(dataList))
-    compare_past_dates(dataList, 30)
-    # dataList = convert_price_to_delta(dataList)
+    convert_price_to_delta(dataList)
+    compare_past_dates(dataList, 10)
     # for data in dataList:
-    #    print(data[0].split("-")[1] + "-" + data[0].split("-")[2])
 
 
 if __name__ == "__main__":
